3c464520e784936c1469c3fe593374be3a5c2c8e
Mateo-RENAC <mateo.renac@gmail.com>
2024-03-26 10:29:40 +0100

    Ajoutez des fichiers projet.

---
+++ Animal.cs
@@ -0,0 +1,83 @@
+﻿class Animal
+{
+    public string? Name;
+
+    public void Moove()
+    {
+        if (Exist(Name))
+        {
+            Console.WriteLine(Name + " I'm mooving !");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("NoneExistAnimal is mooving");
+            Console.ReadLine();
+        }
+    }
+
+    public void Sleep()
+    {
+        if (Exist(Name))
+        {
+            Console.WriteLine(Name + " Zzz...");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("NoneExistAnimal is sleeping\n");
+            Console.ReadLine();
+        }
+    }
+
+    public static bool Exist(String Name)
+    {
+        if (Name == null)
+        {
+            return false;
+        }
+        else { return true; }
+    }
+
+    public Animal(String Name)
+    {
+        this.Name = Name;
+    }
+
+    public Animal()
+    {
+        this.Name = null;
+    }
+
+}
+
+class Wolf:Animal
+{
+    public String Sound()
+    {
+        Console.WriteLine("Aooouh!\n");
+        return "Aooouh!\n";
+
+    }
+
+    public Wolf(String Name)
+    {
+        this.Name = Name;
+    }
+
+}
+
+class Sheep:Animal
+{
+    public String Sound()
+    {
+        Console.WriteLine("Beeeh\n");
+        return "Beeeh\n";
+    }
+
+    public Sheep(String Name)
+    {
+        this.Name = Name;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection.Metadata;
+using static Animal;
+
+
+class Program
+{ 
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Choose Animal : wolf or sheep");
+        String user = Console.ReadLine();
+        if (user.Equals("wolf")) 
+        {
+            Wolf choosing = new Wolf("wolf");
+            choosing.Sound();
+            choosing.Moove();
+            choosing.Sleep();
+            Console.WriteLine("END_FUNCTION");
+        }
+        else if (user.Equals("sheep"))
+        {
+            Sheep choosing = new Sheep("sheep");
+            choosing.Sound();
+            choosing.Moove();
+            choosing.Sleep();
+            Console.WriteLine("END_FUNCTION");
+        }
+        else
+        {
+            Animal animal = new Animal();
+            Console.WriteLine("No_sound");
+            animal.Moove();
+            animal.Sleep();
+            Console.WriteLine("END_FUNCTION");
+        }
+    }
+
+}
