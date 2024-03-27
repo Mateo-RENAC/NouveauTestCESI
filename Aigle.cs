2ab22fe24e95b733fdbf444bd8eb5cd7743600f1
Mateo-RENAC <mateo.renac@gmail.com>
2024-03-27 11:32:30 +0100

    Création des classes Aigles et Bird

---
+++ Aigle.cs
@@ -0,0 +1,12 @@
+﻿class Aigle : Animal
+{
+    public string? Name { get; set; }
+    public int rarity { get; set; }
+
+    public Aigle(string Name, int rarity = 1)
+    {
+        this.Name = Name;
+        this.rarity = 3;
+    }
+
+}
---
+++ Bird.cs
@@ -0,0 +1,9 @@
+﻿class Bird
+{
+    public string? Name { get; set; }
+    public int rarity { get; set; }
+
+    public void Voler() {
+        Console.WriteLine("I belive I can flyyyyyyyy....");
+        }
+}
